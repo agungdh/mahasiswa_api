c2408e813b03b9d9da999f3f399c7039e58e0389
agungdh <agungdh@live.com>
2018-04-29 21:44:04 +0700

    update

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -7,53 +7,64 @@ class Welcome extends CI_Controller {
 		$this->load->model('m_welcome');
 	}
 
-	function ambil_limbah() {
-		$data_limbah = "[";
-		foreach ($this->db->get('limbah')->result() as $item) {
-			$data_limbah .= '"' . $item->limbah . '"' . ", ";
-		}
-		$data_limbah .= "]";
-		// echo json_encode($data);
-		echo $data_limbah;
-	}
-
 	function index() {
-		$halaman_utama = $this->session->level == 1 ? "template/halaman_utama_admin" : "template/halaman_utama_user";
-		// $data['tahun'] = $this->input->get('tahun') ?: date('Y');
-		$data['tahun_masuk'] = $this->input->get('tahun_masuk') ?: date('Y');
-		$data['tahun_keluar'] = $this->input->get('tahun_keluar') ?: date('Y');
-	
-		$data['limbah'] = "[";
-		foreach ($this->db->get('limbah')->result() as $item) {
-			$data['limbah'] .= '"' . $item->limbah . '"' . ", ";
+		$data['mahasiswa'] = $this->m_welcome->ambil();
+		
+		$get_id = $this->input->get('id');
+		if ($get_id != null && $get_id != "") {
+			$data['form'] = $this->m_welcome->ambil_where(array('id' => $get_id));
 		}
-		$data['limbah'] .= "]";
-
-		$this->session->login != true ? $this->load->view("template/halaman_login") : $this->load->view('template/template',array("isi" => $halaman_utama, "data" => $data));
+		
+		$this->load->view('welcome/index', $data);
 	}
 
-	function aksi_login() {
-		$data_user = $this->m_welcome->cek_login($this->input->post('username'), hash('sha512', $this->input->post('password')));
-		if ($data_user != null) {
+	function tambah() {
+		$where['npm'] = $this->input->post('npm');
+		if ($this->m_welcome->ambil_where($where) == null) {
+			$data['npm'] = $this->input->post('npm');
+			$data['nama'] = $this->input->post('nama');
+			$this->m_welcome->tambah($data);			
 			
-			$array_data_user = array(
-				'id'  => $data_user->id,
-				'username'  => $data_user->username,
-				'nama'  => $data_user->nama,
-				'level'  => $data_user->level,
-				'login'  => true
-			);
-
-			if ($data_user->id_unit != null) {
-				$array_data_user['id_unit'] = $data_user->id_unit;
-				$array_data_user['unit'] = $this->m_welcome->ambil_unit_id($data_user->id_unit)->unit;			
-			}
-
-			$this->session->set_userdata($array_data_user);
-
 			redirect(base_url());
 		} else {
 			redirect(base_url('?error=1'));
 		}
 	}
+
+	function ubah() {
+		$mahasiswa_awal = $this->m_welcome->ambil_where(array('id' => $this->input->post('id')));
+
+		if ($mahasiswa_awal->npm != $this->input->post('npm')) {
+			$where['npm'] = $this->input->post('npm');
+			if ($this->m_welcome->ambil_where($where) == null) {
+				$data['npm'] = $this->input->post('npm');
+				$data['nama'] = $this->input->post('nama');
+
+				$where2['id'] = $this->input->post('id');
+
+				$this->m_welcome->ubah($data, $where2);			
+				
+				redirect(base_url());
+			} else {
+				redirect(base_url('?error=1'));
+			}
+		} else {
+			$data['npm'] = $this->input->post('npm');
+			$data['nama'] = $this->input->post('nama');
+
+			$where2['id'] = $this->input->post('id');
+
+			$this->m_welcome->ubah($data, $where2);			
+			
+			redirect(base_url());
+		}
+	}
+
+	function hapus($id) {
+		$where['id'] = $id;
+		$this->m_welcome->hapus($where);
+
+		redirect(base_url());
+	}
+
 }
